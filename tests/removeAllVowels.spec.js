014b34792fb0af88b6a6b6a976c0c525404aefe2
tejbans <singh.tejbans@gmail.com>
2016-08-01 13:43:38 -0700

    Done.

--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,9 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+it('should remove all vowels', function () {
+    var results = $filter('removeAllVowels')('asodnasjdndaflnk');
+
+    expect(results).toEqual('sdnsjdndflnk');
+  });
 });
